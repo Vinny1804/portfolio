b662ba167bb44d8446e21a88f5780590f7cfa4f5
Vinicius <vinny_aguiar@hotmail.com>
2025-02-15 23:30:30 -0300

    :construction: Em progresso: Começando layout na pagina Sobre

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,11 @@
 import s from './footer.module.scss'
+import githubIconFooter from '../../assets/githubIconFooter.png'
 
 export default function Footer() {
     return (
         <footer className={s.footer}>
             <p>Desenvolvido por Vinicius Aguiar</p>
+            <a target='blank' href='https://github.com/Vinny1804/portfolio'><img src={githubIconFooter} alt="Icone do Github" /></a>
         </footer>
     )
 }
--- src/pages/sobre/Sobre.jsx
+++ src/pages/sobre/Sobre.jsx
@@ -1,9 +1,49 @@
-
+import { useState } from 'react'
+import linkedinLogo from '../../assets/linkedinLogo.png'
+import githubLogo from '../../assets/githubLogo.png'
+import instagramLogo from '../../assets/instagramLogo.png'
+import downloadIcon from '../../assets/downloadIcon.png'
+import s from './sobre.module.scss'
 
 export default function Sobre() {
+
+    const [marcado, setMarcado] = useState(false)
+    const mudar = () => {
+        setMarcado(!marcado)
+    }
+
+    const minhasRedes = (
+        <section className={s.minhasRedesLogos}>
+            <a target='blank' href="https://www.linkedin.com/in/vinicius-bernardino/"><img src={linkedinLogo} alt="Logo azul do LinkedIn" /></a>
+            <a target='blank' href="https://github.com/Vinny1804"><img src={githubLogo} alt="Logo do GitHub" /></a>
+            <a target='blank' href="https://www.instagram.com/vinnyaguiarr/"><img src={instagramLogo} alt="Logo do Instagram" /></a>
+        </section>
+    )
+
+    const download = (<img className={s.downloadIcon} src={downloadIcon}/>)
+
+
     return (
-        <section>
-            SOBRE
+        <section className={s.sobreSection}>
+            <h1>Sobre Mim</h1>
+            <p>Olá! Me chamo Vinicius Aguiar, tenho 28 anos e sou estudante de Análise e Desenvolvimento de Sistemas. Moro em São Paulo, Brasil, e estou iniciando minha jornada na área de tecnologia. Tenho interesse em atuar como Full Stack, explorando tanto o front-end quanto o back-end, e estou sempre me atualizando para expandir meu conhecimento e aprimorar minhas habilidades na área.</p>
+
+            <div className={s.menuMinhasRedes}>
+
+                <label className={s.checkboxContainer}>
+                    <input
+                    type="checkbox"
+                    checked={marcado}
+                    onChange={mudar}/>
+                    <span className="checkmark">Minhas Redes</span>
+                </label>
+                <button class={s.button} type="button">
+                    <span class={s.buttonText}>Currículo</span>
+                    <span class={s.buttonIcon}>{download}</span>
+                    </button>
+                <>{marcado ? minhasRedes : ""}</>
+
+            </div>
         </section>
     )
 }
